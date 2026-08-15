fd1c4428009dcfd2ecf8e9046defaa6747abd2fe
rezax250 <ahmadalireza2005@gmail.com>
2026-08-15 08:48:11 -0400

    feat: add countdown mode with shared Timer struct

--- src/ui.h
+++ src/ui.h
@@ -2,9 +2,10 @@
 #define UI_H_
 
 #include "common.h"
+#include "timer.h"
 
 int ui_init(void);
-void ui_render(double elapsed);
+void ui_render(double elapsed, Timer *timer);
 void ui_shutdown(void);
 
 #endif
